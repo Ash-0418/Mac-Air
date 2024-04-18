310e07cefbe8f16f9bb84cd5e2829fd88938fb0e
ASH <58816483+Ash-0418@users.noreply.github.com>
2024-04-18 22:30:13 +0900

    #13

--- src/client/js/recoder.js
+++ src/client/js/recoder.js
@@ -1,7 +1,53 @@
 const startBtn = document.getElementById("startBtn");
+const preview = document.getElementById("preview");
 
-handelStart = () =>{
+let stream;
+let recoder;
+let videoFile;
 
+const handleDownloaded = (event) =>{
+    const a = document.createElement("a");
+    a.href = videoFile;
+    a.download = "MyRecordgin.webm";
+    document.body.appendChild(a);
+    a.click();
 };
 
-startBtn.addEventListener("click", handelStart);
+const handleStop = () =>{
+    startBtn.innerText = "Download recoding"
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleDownloaded);
+    recoder.stop();
+};
+
+const handleStart = () =>{
+    startBtn.innerText = "Stop recoding"
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+    recoder = new MediaRecorder(stream);
+    recoder.ondataavailable = (event) => {
+        videoFile =URL.createObjectURL(event.data);
+        video.srcObject = null;
+        video.src = videoFile;
+        video.loop();
+        video.play();
+    };
+    recoder.start();
+    setTimeout(() =>{
+        recoder.stop();
+    },10000);
+    
+};
+
+
+const init = async() =>{
+    stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {width: 200, height:200},
+    });
+    preview.srcObject = stream;
+    preview.play();
+};
+init();
+
+startBtn.addEventListener("click", handleStart);
